acfea06d3e5523c613bff8fd70846df04d18cf96
MousSeven <Mous.Projet.Android@gmail.com>
2015-05-11 09:59:39 +0200

    Création de la class Unitee.

---
+++ core/src/model/metier/Unitee.java
@@ -0,0 +1,14 @@
+package model.metier;
+
+import java.io.Serializable;
+
+/**
+ * Description : Class Unitee qui compose les armées
+ */
+public class Unitee implements Serializable{
+	
+	public Unitee() {
+		
+	}
+
+}
