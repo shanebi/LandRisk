7ef352f37ff617694fe404e155041a0d03273f58
MousSeven <Mous.Projet.Android@gmail.com>
2015-05-15 10:19:47 +0200

    Modification de la classe Vue.

--- core/src/vue/ConsoleVue.java
+++ core/src/vue/ConsoleVue.java
@@ -16,8 +16,33 @@ public class ConsoleVue implements Vue{
 		ConsoleVue cv = null;
 		cv = new ConsoleVue();
 		cv.enregistrerAuManager();
-	}
+		
+		//----- TEST 1 A SUP --- Création de la partie
+		Manager.getInstance().nouveauJeuEnLocal();
+		
+		
+		//----- TEST 2 A SUP JOUER ---- Jouer Au jeu
+		Manager.getInstance().jouer();
+		
+		
+		System.out.println("--- TEST DU Dé----");
+		//----- TEST 3 A SUP BENJAMEN ----  lancer dé
+		Manager.getInstance().LanceDe();
+		System.out.println("Modifier valeur dé et relancer : ");
+		Manager.getInstance().ModifierNbFaceDice(100);
+		Manager.getInstance().LanceDe();
+		
+		
+		
 
+		
+		
+		
+		
+		
+		
+	}
+	
 	//enregistrer à la vue
 	private void enregistrerAuManager() {
 		Manager.getInstance().setVue(this);
