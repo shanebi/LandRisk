e3aafef967fc01b3e7001e2753c7668e49c244d0
trynobass <contact@trynobass.fr>
2015-05-21 21:02:15 +0200

    Ajout de la classe pour les reglages de la partie.

---
+++ core/src/vue/screen/ParamPartie.java
@@ -0,0 +1,58 @@
+package vue.screen;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Ecran où le joueur la carte de jeu et sa couleur
+ * @author Benjamin Bertrand
+ *
+ */
+public class ParamPartie implements Screen {
+
+	public ParamPartie() {
+	
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
